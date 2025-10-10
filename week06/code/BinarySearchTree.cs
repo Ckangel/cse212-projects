0b1093ae1c0708c32d487b90bed9f45583ca8402
Ckangel <ckajeigbe1@gmail.com>
2025-10-10 20:13:20 +0000

    trees_tests of week 6 passed

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -1,106 +1,72 @@
 using System.Collections;
+using System.Collections.Generic;
 
 public class BinarySearchTree : IEnumerable<int>
 {
     private Node? _root;
 
-    /// <summary>
-    /// Insert a new node in the BST.
-    /// </summary>
     public void Insert(int value)
     {
-        // Create new node
-        Node newNode = new(value);
-        // If the list is empty, then point both head and tail to the new node.
         if (_root is null)
-        {
-            _root = newNode;
-        }
-        // If the list is not empty, then only head will be affected.
+            _root = new Node(value);
         else
-        {
             _root.Insert(value);
-        }
     }
 
-    /// <summary>
-    /// Check to see if the tree contains a certain value
-    /// </summary>
-    /// <param name="value">The value to look for</param>
-    /// <returns>true if found, otherwise false</returns>
     public bool Contains(int value)
     {
-        return _root != null && _root.Contains(value);
+        return _root?.Contains(value) ?? false;
     }
 
-    /// <summary>
-    /// Yields all values in the tree
-    /// </summary>
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        // call the generic version of the method
-        return GetEnumerator();
-    }
-
-    /// <summary>
-    /// Iterate forward through the BST
-    /// </summary>
-    public IEnumerator<int> GetEnumerator()
-    {
-        var numbers = new List<int>();
-        TraverseForward(_root, numbers);
-        foreach (var number in numbers)
-        {
-            yield return number;
-        }
-    }
-
-    private void TraverseForward(Node? node, List<int> values)
-    {
-        if (node is not null)
-        {
-            TraverseForward(node.Left, values);
-            values.Add(node.Data);
-            TraverseForward(node.Right, values);
-        }
-    }
-
-    /// <summary>
-    /// Iterate backward through the BST.
-    /// </summary>
-    public IEnumerable Reverse()
-    {
-        var numbers = new List<int>();
-        TraverseBackward(_root, numbers);
-        foreach (var number in numbers)
-        {
-            yield return number;
-        }
-    }
-
-    private void TraverseBackward(Node? node, List<int> values)
-    {
-        // TODO Problem 3
-    }
-
-    /// <summary>
-    /// Get the height of the tree
-    /// </summary>
     public int GetHeight()
     {
-        if (_root is null)
-            return 0;
-        return _root.GetHeight();
+        return _root?.GetHeight() ?? 0;
     }
 
     public override string ToString()
     {
         return "<Bst>{" + string.Join(", ", this) + "}";
     }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        var values = new List<int>();
+        TraverseForward(_root, values);
+        foreach (var value in values)
+            yield return value;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private void TraverseForward(Node? node, List<int> values)
+    {
+        if (node is null) return;
+        TraverseForward(node.Left, values);
+        values.Add(node.Data);
+        TraverseForward(node.Right, values);
+    }
+
+    public IEnumerable Reverse()
+    {
+        var values = new List<int>();
+        TraverseBackward(_root, values);
+        foreach (var value in values)
+            yield return value;
+    }
+
+    private void TraverseBackward(Node? node, List<int> values)
+    {
+        if (node is null) return;
+        TraverseBackward(node.Right, values);
+        values.Add(node.Data);
+        TraverseBackward(node.Left, values);
+    }
 }
 
-public static class IntArrayExtensionMethods {
-    public static string AsString(this IEnumerable array) {
+public static class IntArrayExtensionMethods
+{
+    public static string AsString(this IEnumerable array)
+    {
         return "<IEnumerable>{" + string.Join(", ", array.Cast<int>()) + "}";
     }
 }
